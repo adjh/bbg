6ff9e3cc321e6a0b92257e16fa9f4b7c325afaa2
Raecoo Cao <raecoo@gmail.com>
2010-01-06 11:55:17 +0800

    update sphinx config file

--- config/environment.rb
+++ config/environment.rb
@@ -10,8 +10,8 @@ Rails::Initializer.run do |config|
 
   config.gem 'authlogic', :lib => 'haml', :source => 'http://gems.github.com'
   config.gem 'haml',      :lib => 'haml', :source => 'http://gems.github.com'
-  config.gem 'thoughtbot-paperclip', :lib => 'paperclip',     :source => 'http://gems.github.com'
-  config.gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
+  config.gem 'paperclip', :lib => 'paperclip',     :source => 'http://gems.github.com'
+  config.gem 'will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
   
   # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
