76d56b159597e9d6b53ab6e379ca03c1d3ee0f3b
Raecoo Cao <raecoo@gmail.com>
2010-01-15 16:21:01 +0800

    added create wine form

--- app/controllers/admin/wines_controller.rb
+++ app/controllers/admin/wines_controller.rb
@@ -1,3 +1,7 @@
 class Admin::WinesController < Admin::BaseController
   set_tab :wines
+  
+  def new
+    @wine = Wine.new
+  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -8,11 +8,11 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
-  config.gem 'authlogic', :lib => 'haml', :source => 'http://gems.github.com'
+  config.gem 'authlogic', :lib => 'authlogic', :source => 'http://gems.github.com'
   config.gem 'haml',      :lib => 'haml', :source => 'http://gems.github.com'
   config.gem 'paperclip', :lib => 'paperclip',     :source => 'http://gems.github.com'
   config.gem 'will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
-  
+  config.gem 'formtastic', :source => 'http://gems.github.com'
   # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 
