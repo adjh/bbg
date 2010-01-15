2fdbb8fcedf93b3ff10f9c5791f2ded948b13c2f
Raecoo Cao <raecoo@gmail.com>
2010-01-15 16:42:55 +0800

    added grape & winery form

--- app/controllers/admin/grapes_controller.rb
+++ app/controllers/admin/grapes_controller.rb
@@ -1,3 +1,7 @@
 class Admin::GrapesController < Admin::BaseController
   set_tab :wines
+  
+  def new
+    @grape  = Grape.new
+  end
 end
--- app/controllers/admin/wineries_controller.rb
+++ app/controllers/admin/wineries_controller.rb
@@ -1,3 +1,7 @@
 class Admin::WineriesController < Admin::BaseController
   set_tab :wines
+  
+  def new
+    @winery = Winery.new
+  end
 end
